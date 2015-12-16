198f76a5ab3224c4569dc1aa41639d6dc8808eb5
{Krzysztof Studniarek} <{krzysztof.studniarek@mbank.pl}>
2015-12-16 11:26:08 +0100

    WSAD controls added

--- scripts/game.js
+++ scripts/game.js
@@ -114,25 +114,25 @@ function update()
 
 function controls ()
 {
-	if(pressed[KEY_UP] && hero.platform != null){
+	if((pressed[KEY_UP] || pressed[KEY_W] )&& hero.platform != null){
 		hero.platform = null;
 		hero.vy = -6;
 		hero.y -= 1;
 	}
 	
-	if(pressed[KEY_LEFT] || goingLeft){
+	if(pressed[KEY_LEFT] || pressed[KEY_A] || goingLeft){
 		goingLeft = true;
 		hero.vx = -2;
 	}
-	if(released[KEY_LEFT]){
+	if(released[KEY_LEFT] || released[KEY_A]){
 		goingLeft = false;
 	}
 	
-	if(pressed[KEY_RIGHT] || goingRight){
+	if(pressed[KEY_RIGHT] || pressed[KEY_D] || goingRight){
 		goingRight = true;
 		hero.vx = 2;
 	}
-	if(released[KEY_RIGHT]){
+	if(released[KEY_RIGHT] || released[KEY_D]){
 		goingRight = false;
 	}
 	
