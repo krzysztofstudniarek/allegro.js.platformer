c50deb80bbd820bb50ee0328272e239e39a57d1a
{Krzysztof Studniarek} <{krzysztof.studniarek@mbank.pl}>
2015-12-17 12:36:04 +0100

    comments in control.js file added

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,27 +1,36 @@
+//Function for handling controls
 function controls ()
 {
+	//jumping
 	if((pressed[KEY_UP] || pressed[KEY_W] )&& hero.platform != null){
 		hero.platform = null;
 		hero.vy = -6;
 		hero.y -= 1;
 	}
 	
+	//moving left
 	if(pressed[KEY_LEFT] || pressed[KEY_A] || goingLeft){
 		goingLeft = true;
 		hero.vx = -2;
 	}
+	
+	//stop moving left
 	if(released[KEY_LEFT] || released[KEY_A]){
 		goingLeft = false;
 	}
 	
+	//moving right
 	if(pressed[KEY_RIGHT] || pressed[KEY_D] || goingRight){
 		goingRight = true;
 		hero.vx = 2;
 	}
+	
+	//stop moving right
 	if(released[KEY_RIGHT] || released[KEY_D]){
 		goingRight = false;
 	}
 	
+	//shoot on mouse LPM press
 	if(mouse_pressed){
 		var d = distance(hero.x +hero.width/2, hero.y + hero.height/2, mouse_x, mouse_y);
 		bullets.add({
